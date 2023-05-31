dda6408cd9090331a2dd115fbbd592cd252aa584
omar battistotti <omar.battistotti@webscience.it>
2023-05-31 14:40:03 +0200

    .

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from edl.passwordUtils import *
 from edl.generateKey import *
 from edl.encrypt import *
-
+import sys
     
 '''
 begin the function
@@ -18,8 +18,11 @@ ask for all the information
 ask_for_pwd(encrObj)
 as_for_opt(encrObj)
 generate_key(encrObj)
-encrObj.filename = input('file name: ')
 
+if sys.argv is None or len(sys.argv) <= 1:
+    encrObj.filename = input('file name: ')
+else:
+    encrObj.filename = sys.argv[1]
 '''
 iterate until the file is encrypted or decrypted
 '''
